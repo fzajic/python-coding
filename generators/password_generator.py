193b55c89eabd76d7bcf9f5d5e13f0aa45556096
fzajic <filip.zajic007@gmail.com>
2019-04-25 08:13:07 +0000

    generators commit

--- generators/password_generator.py
+++ generators/password_generator.py
@@ -4,7 +4,7 @@ import random
 fout = open("hesla.txt", "w")
 for i in range(20):
   s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
-  passlen = 3
+  passlen = 8
   p =  "".join(random.sample(s,passlen ))
   fout.write(str(p) + "\n")
 fout.close()
