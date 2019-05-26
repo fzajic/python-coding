8710dbf116d07a81e2aca546d58e5e540f6b07cd
fzajic <filip.zajic007@gmail.com>
2019-05-26 20:19:18 +0200

    ratatouille commit 0.13

--- the_ultimate_ratatouille_recipe/function_cookbook.py
+++ the_ultimate_ratatouille_recipe/function_cookbook.py
@@ -6,16 +6,12 @@ def setup():
 def read_data_input(file_name):
 
     fin = open(file_name, "r")
-<<<<<<< HEAD
     data = fin.readline().split()
     return data
 
-=======
     numbers = ""
     for i in range(4):
         numbers = numbers + fin.readline() + ""
     return numbers.split()
 
 setup()
->>>>>>> 8a918f200c19864bbb737b77f08056366633869b
-print(read_data_input("input_data.txt"))
---
+++ the_ultimate_ratatouille_recipe/ratatouille_recipe.py
@@ -0,0 +1,3 @@
+import os
+os.chdir("the_ultimate_ratatouille_recipe")
+from function_cookbook import setup, read_data_input
