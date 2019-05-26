81ebd88d47862818127b2d79ad2a2ba368a0ad18
fzajic <filip.zajic007@gmail.com>
2019-05-26 19:41:41 +0200

    ratatouille commit 0.12 merge

--- the_ultimate_ratatouille_recipe/function_cookbook.py
+++ the_ultimate_ratatouille_recipe/function_cookbook.py
@@ -1,10 +1,6 @@
 def read_data_input(file_name):
     fin = open(file_name, "r")
-    data = []
-    for i in range(4):
-        data.append(fin.readlines())
-    fin.close()
+    data = fin.readline().split()
     return data
-
 
 print(read_data_input("input_data.txt"))
