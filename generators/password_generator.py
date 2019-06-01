e67dbf30099ee2df1061c3f86591b21f1de803ba
fzajic <filip.zajic007@gmail.com>
2019-06-01 15:32:25 +0200

    generators commit 1.0

--- generators/password_generator.py
+++ generators/password_generator.py
@@ -1,8 +1,5 @@
 # generate a password with length "passlen" with no duplicate characters in the password
-<<<<<<< HEAD
 
-=======
->>>>>>> aef0e5d23c1d0274d86ceaa9c1c0a06007377a10
 import random, os
 os.chdir("generators")
 fout = open("hesla.txt", "w")
