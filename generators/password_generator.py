aef0e5d23c1d0274d86ceaa9c1c0a06007377a10
fzajic <filip.zajic007@gmail.com>
2019-06-01 10:30:25 +0200

    ratatouille commit 0.21 udelal jsem max a min funkce vylepsil password generator

--- generators/password_generator.py
+++ generators/password_generator.py
@@ -1,6 +1,6 @@
 # generate a password with length "passlen" with no duplicate characters in the password
-
-import random
+import random, os
+os.chdir("generators")
 fout = open("hesla.txt", "w")
 for i in range(20):
   s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
--- the_ultimate_ratatouille_recipe/ratatouille_recipe.py
+++ the_ultimate_ratatouille_recipe/ratatouille_recipe.py
@@ -1,3 +1,3 @@
 import os
 os.chdir("the_ultimate_ratatouille_recipe")
-from function_cookbook import setup, read_data_input
+from function_cookbook import setup, read_data_input, max, min
