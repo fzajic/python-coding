ecc1b6ddea3ae2d1b178e53b9f890d1b50f0c9da
fzajic <filip.zajic007@gmail.com>
2019-06-01 15:29:56 +0200

    ratatouille commit 0.21

--- generators/password_generator.py
+++ generators/password_generator.py
@@ -1,10 +1,11 @@
 # generate a password with length "passlen" with no duplicate characters in the password
 
-import random
+import random, os
+os.chdir("generators")
 fout = open("hesla.txt", "w")
 for i in range(20):
   s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
-  passlen = 8
+  passlen = 20
   p =  "".join(random.sample(s,passlen ))
   fout.write(str(p) + "\n")
 fout.close()
