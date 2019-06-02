9a0457abb0f28445dae4953cd4c92a0a5149a1d3
fzajic <filip.zajic007@gmail.com>
2019-06-02 18:48:04 +0200

    ratatouille commit 0.22 tenhle commit delam kvuli presunu souboru na ssd nahoda je blbec (stejne jako ja), takze proto :)

--- the_ultimate_ratatouille_recipe/function_cookbook.py
+++ the_ultimate_ratatouille_recipe/function_cookbook.py
@@ -1,15 +1,20 @@
 def setup():
     import os
-    os.chdir("the_ultimate_ratatouille_recipe")
+    if os.getcwd() != r"D:\Coding\python-coding\the_ultimate_ratatouille_recipe":
+        os.chdir("the_ultimate_ratatouille_recipe")
+        print("changing directorey to: \n", os.getcwd(), "\n", "done... \n")
+    else:
+        print("directory is already set correctly, no need to change it")
     return
-setup()
 
+setup()
 
 def file_length(input_file):
     fin = open(input_file, "r")
     l = 0
     for i in fin:
         l = l+1
+    fin.close()
     return l
 
 def first_lines(input_file):
@@ -26,42 +31,71 @@ def first_lines(input_file):
         number_of_case_lines = 2 + first_number                             #get case length
         case_lengths.append(number_of_case_lines)
         current_line = current_line + number_of_case_lines
+    fin.close()
     return first_lines, case_lengths
 
 def make_integers(line):
-    for i in range(len(line)):
-        #print(i)
+    for i in range(len(line)):                             #return
         line[i] = int(line[i])
     return line
 
 def read_data_input(input_file, case_number):               #return number of line of first information of given case number
+
     from linecache import getline
     begin_line = first_lines(input_file)[0][case_number-1]  #gets first line of case
     case_length = first_lines(input_file)[1][case_number-1] #gets length of case in number of lines
+    data_list = []
     for i in range(begin_line, begin_line + case_length):
         line = getline(input_file, i).split()               #make list of numbers from line
         line = make_integers(line)                          #change strings to integers
-        print(line)
-    return
+        data_list.append(line)
+    return data_list
 
-def max(recipe_value):
-    recipe_value = float(recipe_value)
-    max = recipe_value * 1.1
+def max(recipe_ingredient_value):
+    """Returns maximal value of ingredient:
+    Parameters
+    ----------
+    recipe_ingredient_value:
+        number of grmas needed for one serving (according to recipe)
+
+    Returns
+    -------
+        maximal usable value of ingredient
+    """
+
+    recipe_ingredient_value = float(recipe_ingredient_value)
+    max = recipe_ingredient_value * 1.1
     return max
+pri
 
-def min(recipe_value):
-    recipe_value = float(recipe_value)
-    min = recipe_value * 0.9
+def min(recipe_ingredient_value):
+    recipe_ingredient_value = float(recipe_ingredient_value)
+    min = recipe_ingredient_value * 0.9
     return min
 
-def limit(recipe_value):
-    recipe_value = float(recipe_value)
-    min = recipe_value * 0.9
-    max = recipe_value * 1.1
-    return min, max
-    
+def limit(recipe_ingredient_value, type):
+    """Short summary:
+    Paramters
+    ---------
+    recipe_ingredient_value:
+        number of grams (value)
+    type:
+        either max or min, depending on desired output
 
+    Returns
+    -------
+
+    """
+    recipe_ingredient_value = float(recipe_ingredient_value)
+    min = round(recipe_ingredient_value * 0.9, 3)
+    max = round(recipe_ingredient_value * 1.1, 3)
+    if type == max:
+        return max
+    elif type == min:
+        return min
 
 
 #print(first_lines("data.txt"))
-print(read_data_input("data.txt",6))
+#print(read_data_input("data.txt",6))
+
+#print(limit(100, max))
--- the_ultimate_ratatouille_recipe/joking_file.py
+++ the_ultimate_ratatouille_recipe/joking_file.py
@@ -1,16 +1,19 @@
+import os
+print(os.getcwd())
 def setup():
     import os
     os.chdir("the_ultimate_ratatouille_recipe")
     return
+setup()
+print(os.getcwd())
 
-#setup()
+raise Exception("I am an exception!")
 
-
-
-fin = open("input_data.txt", "r")
+"""
+fin = open("data.txt", "r")
 
 from linecache import getline
-x = getline("input_data.txt", 0)
+x = getline("data.txt", 0)
 print(x)
 print("")
 data = fin.readline().split()
@@ -18,8 +21,9 @@ print(data)
 fin.close()
 
 
-fin = open("input_data.txt", "r") #count number of lines
+fin = open("data.txt", "r") #count number of lines
 l=0
 for i in fin:
     l = l+1
 print(l)
+"""
