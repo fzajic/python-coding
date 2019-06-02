7b334fc0c12dfbe2772646c829bbdb65bf818b20
fzajic <filip.zajic007@gmail.com>
2019-06-02 20:59:39 +0200

    ratatouille commit 0.23 dodelal jsem definice k funkcim a popisky neni to finalni, ale pouzitelne to je

--- the_ultimate_ratatouille_recipe/function_cookbook.py
+++ the_ultimate_ratatouille_recipe/function_cookbook.py
@@ -1,15 +1,31 @@
-def setup():
+def setup(state):
+    """Set enviroment
+    Paramters
+    ---------
+    state:
+        Set False or True in order to get printed info into terminal
+    """
     import os
-    if os.getcwd() != r"D:\Coding\python-coding\the_ultimate_ratatouille_recipe":
+    if os.getcwd() != r"C:\Users\filip\Coding":
         os.chdir("the_ultimate_ratatouille_recipe")
-        print("changing directorey to: \n", os.getcwd(), "\n", "done... \n")
+        if state == True:
+            print("changing directorey to: \n", os.getcwd(), "\n", "done... \n")
     else:
-        print("directory is already set correctly, no need to change it")
+        if state == True:
+            print("directory is already set correctly, no need to change it")
     return
 
-setup()
+
+setup(False)
 
 def file_length(input_file):
+    """return number of lines file
+
+    Paramters:
+    ----------
+    input_file:
+        name of data file
+    """
     fin = open(input_file, "r")
     l = 0
     for i in fin:
@@ -18,6 +34,13 @@ def file_length(input_file):
     return l
 
 def first_lines(input_file):
+    """Return number of first line of each case and length of gap between them
+
+    Paramters:
+    ----------
+    input_file:
+        name of data file
+    """
     from linecache import getline
     fin = open(input_file, "r")
     first_lines = []
@@ -26,33 +49,61 @@ def first_lines(input_file):
     for i in fin:
         if getline(input_file, current_line) == "":
             break
-        first_number = int(getline(input_file, current_line).split()[0])    #get first number of case that determines its length
-        first_lines.append(current_line)                                    #append another begin line of another case
-        number_of_case_lines = 2 + first_number                             #get case length
+        # get first number of case that determinimumes its length
+        first_number = int(getline(input_file, current_line).split()[0])
+        first_lines.append(current_line)  # append another begin line of another case
+        number_of_case_lines = 2 + first_number  # get case length
         case_lengths.append(number_of_case_lines)
         current_line = current_line + number_of_case_lines
     fin.close()
     return first_lines, case_lengths
 
+
 def make_integers(line):
-    for i in range(len(line)):                             #return
+    """Make all components of list integers
+
+    Paramters:
+    ----------
+    line:
+        specific data from line (list)
+
+    """
+    for i in range(len(line)):
         line[i] = int(line[i])
     return line
 
-def read_data_input(input_file, case_number):               #return number of line of first information of given case number
 
+def read_data_input(input_file, case_number):
+    """Return formated data of case from data file
+
+    Paramters:
+    ----------
+    input_file:
+        name of data file
+    case_number:
+        number of case from input_input file, detects specific line automatically (cool right?)
+
+    Returns
+    -------
+    data_list:
+        list of list of each line of case
+        example: [[1, 8], [10], [11, 13, 17, 11, 16, 14, 12, 18]]
+    """
     from linecache import getline
-    begin_line = first_lines(input_file)[0][case_number-1]  #gets first line of case
-    case_length = first_lines(input_file)[1][case_number-1] #gets length of case in number of lines
+    begin_line = first_lines(input_file)[0][case_number-1]  # gets first line of case
+    # gets length of case in number of lines
+    case_length = first_lines(input_file)[1][case_number-1]
     data_list = []
     for i in range(begin_line, begin_line + case_length):
-        line = getline(input_file, i).split()               #make list of numbers from line
-        line = make_integers(line)                          #change strings to integers
+        line = getline(input_file, i).split()  # make list of numbers from line
+        line = make_integers(line)  # change strings to integers
         data_list.append(line)
     return data_list
 
-def max(recipe_ingredient_value):
-    """Returns maximal value of ingredient:
+
+def maximum(recipe_ingredient_value):
+    """Returns maximumimal value of ingredient:
+
     Parameters
     ----------
     recipe_ingredient_value:
@@ -60,42 +111,55 @@ def max(recipe_ingredient_value):
 
     Returns
     -------
-        maximal usable value of ingredient
+        maximumimal usable value of ingredient
     """
 
     recipe_ingredient_value = float(recipe_ingredient_value)
-    max = recipe_ingredient_value * 1.1
-    return max
-pri
+    maximum = recipe_ingredient_value * 1.1
+    return maximum
 
-def min(recipe_ingredient_value):
+
+def minimum(recipe_ingredient_value):
+    """Returns minimal value of ingredient:
+
+    Parameters
+    ----------
+    recipe_ingredient_value:
+        number of grmas needed for one serving (according to recipe)
+
+    Returns
+    -------
+        minimal usable value of ingredient
+    """
     recipe_ingredient_value = float(recipe_ingredient_value)
-    min = recipe_ingredient_value * 0.9
-    return min
+    minimum = recipe_ingredient_value * 0.9
+    return minimum
+
 
 def limit(recipe_ingredient_value, type):
-    """Short summary:
+    """
     Paramters
     ---------
     recipe_ingredient_value:
         number of grams (value)
     type:
-        either max or min, depending on desired output
-
-    Returns
-    -------
-
+        either maximum or minimum, depending on desired output
     """
+
     recipe_ingredient_value = float(recipe_ingredient_value)
-    min = round(recipe_ingredient_value * 0.9, 3)
-    max = round(recipe_ingredient_value * 1.1, 3)
-    if type == max:
-        return max
-    elif type == min:
-        return min
+    minimum = round(recipe_ingredient_value * 0.9, 3)
+    maximum = round(recipe_ingredient_value * 1.1, 3)
+    #print(maximum, minimum)
+    if type == "maximum":
+        return maximum
+    elif type == "minimum":
+        return minimum
 
+"""
+print(first_lines("data.txt"))
+print(read_data_input("data.txt",6))
+print(limit(100, "maximum"))
+"""
 
-#print(first_lines("data.txt"))
-#print(read_data_input("data.txt",6))
-
-#print(limit(100, max))
+print("")
+print(read_data_input("data.txt", 5))
